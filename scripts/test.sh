6d5d03225012424cf75a8f4b7e8600e63693d533
thislooksfun <thislooksfun@repbot.org>
2015-11-30 02:37:49 -0600

    Separated linting

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,7 @@
 EXIT_STATUS=0
 mocha --compilers coffee:coffee-script/register || EXIT_STATUS=$?
-coffeelint -f ./coffeelint.json src test || EXIT_STATUS=$?
+echo "\nlinting 'src' directory:\n"
+coffeelint -f ./coffeelint.json src || EXIT_STATUS=$?
+echo "\nlinting 'test' directory:\n"
+coffeelint -f ./coffeelint.json test || EXIT_STATUS=$?
 exit $EXIT_STATUS
