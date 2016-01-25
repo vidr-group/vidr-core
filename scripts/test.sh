0f35cc9417df8ca1dfa2d6de776da0dcc508a662
thislooksfun <thislooksfun@repbot.org>
2016-01-25 15:34:44 -0600

    Tweaked coverage reporting

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,18 @@
 EXIT_STATUS=0
 echo "Running tests:"
 mocha --recursive --compilers coffee:coffee-script/register --require coffee-coverage/register-istanbul test || EXIT_STATUS=$?
-printf "\nGenerating coverate report... "
-istanbul report || EXIT_STATUS=$?
 echo "\nLinting 'src' directory:\n"
 coffeelint -f ./coffeelint.json src || EXIT_STATUS=$?
 echo "\nLinting 'test' directory:\n"
 coffeelint -f ./coffeelint.json test || EXIT_STATUS=$?
+
+if [ $TRAVIS ]; then
+  printf "\nGenerating coverate report... "
+  istanbul report || EXIT_STATUS=$?
+  echo "Uploading coverage to codeclimate:"
+  codeclimate-test-reporter < ./coverage/lcov.info
+else
+  echo "Not running on travis, skipping coverage\n"
+fi
+
 exit $EXIT_STATUS
