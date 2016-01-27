bc961eb31ab2a31441b826688301b30240964be0
thislooksfun <thislooksfun@repbot.org>
2016-01-26 18:28:00 -0600

    Rearranged test flags

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 EXIT_STATUS=0
 echo "Running tests:"
-mocha --recursive --compilers coffee:coffee-script/register --require coffee-coverage/register-istanbul test || EXIT_STATUS=$?
+mocha --compilers coffee:coffee-script/register test || EXIT_STATUS=$?
 echo "\nLinting 'src' directory:\n"
 coffeelint -f ./coffeelint.json src || EXIT_STATUS=$?
 echo "\nLinting 'test' directory:\n"
