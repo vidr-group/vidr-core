cdc701bd1f4f2c277f0f9240b3bbabadffee3ddb
thislooksfun <thislooksfun@repbot.org>
2016-01-27 16:50:07 -0600

    Let coverage reports be read locally

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,18 +1,22 @@
 EXIT_STATUS=0
+
 echo "Running tests:"
 mocha --compilers coffee:coffee-script/register test || EXIT_STATUS=$?
+
 echo "\nLinting 'src' directory:\n"
 coffeelint -f ./coffeelint.json src || EXIT_STATUS=$?
+
 echo "\nLinting 'test' directory:\n"
 coffeelint -f ./coffeelint.json test || EXIT_STATUS=$?
 
+printf "\nGenerating coverate report... "
+istanbul report || EXIT_STATUS=$?
+
 if [ $TRAVIS ]; then
-  printf "\nGenerating coverate report... "
-  istanbul report || EXIT_STATUS=$?
   echo "\nUploading coverage to codeclimate:"
   codeclimate-test-reporter < ./coverage/lcov.info
 else
-  echo "Not running on travis, skipping coverage\n"
+  echo "Not running on travis, skipping coverage upload\n"
 fi
 
 exit $EXIT_STATUS
